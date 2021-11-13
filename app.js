304d33dd90127b92f73b52232f69dbc1f2e2852d
AshvathMurthy <92076903+AshvathMurthy@users.noreply.github.com>
2021-11-13 11:02:24 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+     <Msg />
+     <Msg />
+     <Msg />
+    </div>
+  );
+}
+
+
+function Msg() {
+  return (
+    <div>
+      <h1> hello,🌐</h1>
+
+      </div>
+
+  );
+}
